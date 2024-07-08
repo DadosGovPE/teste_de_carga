a21fcb6c5cd14752ea6b4eb4a263e587db505515
Carlos Silva <silvaugusto222@gmail.com>
2024-07-08 11:11:57 -0300

    update: add log in csv

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,38 @@
-from locust import HttpUser, task, between
-
+import pandas as pd
+import time
+from locust import HttpUser, task, between, events, run_single_user
+from datetime import datetime
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 3)
+    host = "https://ouvirparamudar-homologacao.pe.gov.br"
 
-    @task(5)
+    @task
     def index(self):
-        self.client.get("/")
+        pass
+
+    def log_server_health(self):
+        response = self.client.get("/")
+        data = {
+            "timestamp": [datetime.now()],
+            "status_code": [response.status_code],
+            "response_time": [response.elapsed.total_seconds()],
+            "content_length": [len(response.content)]
+        }
+        print('Dados:', data)
+        df = pd.DataFrame(data)
+        df.to_csv('server_health.csv', mode='a', index=False, header=False)
+
+@events.test_start.add_listener
+def on_test_start(environment, **kwargs):
+    user = WebsiteUser(environment)
+    user.log_server_health()
+    environment.runner.quit()  # Adiciona um encerramento do Locust após o log
+
+# To add headers to the CSV initially
+if __name__ == "__main__":
+    columns = ["timestamp", "status_code", "response_time", "content_length"]
+    df = pd.DataFrame(columns=columns)
+    df.to_csv('server_health.csv', index=False)
+
+    run_single_user(WebsiteUser)
